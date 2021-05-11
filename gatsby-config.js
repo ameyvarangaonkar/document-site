afca325a5bce460f18e970bb07a834da1201852a
ameyvarangaonkar <ameypv@gmail.com>
2021-05-11 14:18:05 +0530

    Update config parameters

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Document by Code Bushi`,
-    name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
+    title: `RudderStack`,
+    name: `RudderStack`,
+    siteUrl: `https://rudderstack.com/`,
+    description: `Open-source Customer Data Platform`,
     social: [
       {
         name: `github`,
