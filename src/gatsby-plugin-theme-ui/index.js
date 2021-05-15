42c8c9ad4ad0d7c93353b4850fdc14b7a05d34b4
ameyvarangaonkar <ameypv@gmail.com>
2021-05-15 13:51:00 +0530

    Update index.js

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -4,7 +4,7 @@ import headings from "./headings";
 
 const transition = "0.2s ease-out";
 const systemFonts =
-  "Lato";
+  "-apple-system, BlinkMacSystemFont, San Francisco, Helvetica Neue, Helvetica, Ubuntu, Roboto, Noto, Segoe UI, Arial, sans-serif";
 
 export default {
   initialColorMode: `dark`,
